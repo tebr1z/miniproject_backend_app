6ec1cb680805733152a36a2454f20545e67a8673
tebr1z <tabrizyh@code.edu.az>
2024-07-27 23:21:57 +0400

    Feat : branch

--- Juan/obj/Debug/net8.0/Juan.AssemblyInfo.cs
+++ Juan/obj/Debug/net8.0/Juan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Juan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4dcc46729215378715d5e8b0672614030a9a22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fc57bff6d87619c31dd3bd1652d47ec2f6fedd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Juan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Juan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
