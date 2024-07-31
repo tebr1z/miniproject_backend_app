68d68957a52ad7b4fe18584fe012c5a1913c9549
tebr1z <tabrizyh@code.edu.az>
2024-07-31 18:52:17 +0400

    Bug fixed

--- Juan/obj/Debug/net8.0/Juan.AssemblyInfo.cs
+++ Juan/obj/Debug/net8.0/Juan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Juan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac82dde1f67e74b460b26682cfaf657865c8ce90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadd8971e58ff32a81dad967d5ee948e8e74393e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Juan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Juan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
